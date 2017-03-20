82de6e17b9425d77d1e0ff0223a8d637c9db07f8
mouadh ben salem <mouadh.bensalem@716solutions.com>
2017-03-21 00:00:01 +0100

    Revert "716sformation"
    This reverts commit 244756691f0f318ecb4e5087d57070dc53840459.

---
+++ Android4Educ/ExerciceLayout/app/src/main/java/mou3adh/exercicelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package mou3adh.exercicelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
